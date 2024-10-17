9dd4a873962d846220b6acedac567379f7f16aec
Erin2026 <Perrine.wurtz@gmail.com>
2024-10-17 23:03:29 +0200

    tried getDeterminant (not solved)

--- TD2/exercice2/MatrixNumerical.h
+++ TD2/exercice2/MatrixNumerical.h
@@ -13,6 +13,9 @@ class MatrixNumerical : public MatrixBase<T>
 public:
     MatrixNumerical();
     MatrixNumerical(size_t _rows, size_t _cols);
+
+    /*friend T getDeterminant(const MatrixNumerical<T> Matrix);
+    MatrixNumerical<T> getSmaller(); /*besoin pour getDeterminant*/ 
 };
 
 template<typename T>
@@ -24,5 +27,31 @@ template<typename T>
 MatrixNumerical<T>::MatrixNumerical(size_t _rows, size_t _cols):MatrixBase<T>(_rows,_cols){
 
 }
+
+template<typename T>
+T MatrixNumerical<T>::getSmaller(t_size i){
+    MatrixNumerical<int> newMatrix = MatrixNumerical<int>(this.rows-1,this.cols-1)
+    for (int j=0;j<this;j++){}
+}
+
+/*
+template<typename T>
+T MatrixNumerical<T>::getDeterminant(const MatrixNumerical<T> Matrix){
+    int det=0;
+    int power=1;
+    if (Matrix.cols==Matrix.rows){
+        for (int i; i<Matrix.cols;i++);{
+            det+=det + -1*power*this.data[0][i]*getDeterminant(Matrix.getSmaller(i))
+            power=-power;
+        }
+
+    }else{
+        cout<<"on ne peut pas calculer le déterminant d'une matrice qui n'est pas carrée."
+    }
+}
+
+---> erreurs de templates et de types
+*/
+
 
 #endif
